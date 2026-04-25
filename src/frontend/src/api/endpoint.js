fb601b87fdfde6b20815ab2349dad39233c9dcbc
Your Name <you@example.com>
2026-04-25 19:01:17 +0530

    commit before updates

--- src/frontend/src/api/endpoint.js
+++ src/frontend/src/api/endpoint.js
@@ -1,6 +1,6 @@
 // API Base URL
-// export const BASE_URL = "http://localhost:7000";
-export const BASE_URL = "https://api.jpme.in";
+export const BASE_URL = "http://localhost:7000";
+// export const BASE_URL = "https://api.jpme.in";
 
 // API Endpoints
 export const ENDPOINTS = {
@@ -8,8 +8,8 @@ export const ENDPOINTS = {
   LOGIN: "/admin/auth/login",
 
   // Categories
-  ADD_CATEGORY: "/admin/categories",
-  EDIT_CATEGORY: "/admin/categories/:id",
+  ADD_CATEGORY: "/admin/categories/add-category",
+  EDIT_CATEGORY: "/admin/categories/edit-category/:id",
   GET_CATEGORIES: "/admin/categories/get-category",
 };
 
